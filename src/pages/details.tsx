b31c944e9c7015099397aa537366e8b942f04ea2
Oreoluwa <awuloero13@gmail.com>
2024-03-27 22:36:39 +0100

    :sparkles: deployment

--- src/pages/details.tsx
+++ src/pages/details.tsx
@@ -1,6 +1,5 @@
 import DetailsPage from "@/components/UI/details";
 import Layout from "@/components/UI/layout";
-import CreateForm from "@/components/create-form";
 
 export default function Details() {
   return (
